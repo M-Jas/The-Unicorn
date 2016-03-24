1b052528f7f75429e79190f3d55e7739368cba21
Michael Jasinski <majasinski8@gmail.com>
2016-03-24 17:33:17 -0500

    Edit search bar style

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -77,12 +77,15 @@ angular.module('starter.controllers', [])
        searchItem = $scope.search;
       $http.get("http://api.giphy.com/v1/gifs/search?&api_key=dc6zaTOxFJmzC&limit=100&q=" + searchItem).then(function(resp) {
          for (var i = 0; i < resp.data.data.length; i++) {
+           $scope.response = resp
+           console.log($scope.response);
            $scope.photos.push(resp.data.data[i].images.original.url);
          }
       }, function(err) {
         console.error('ERR', err);
       });
    }
+
 
    $scope.showImages = function(index) {
  		  $scope.activeSlide = index;
